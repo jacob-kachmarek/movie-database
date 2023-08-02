b046f71dd66cafec5c30d45bfec510afea0d2fe8
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-08-02 13:08:11 -0700

    npm installs for express and mysql2 and modeled our data structure

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS movies_db;
+CREATE DATABASE movies_db;
+
+USE movies_db;
+
+CREATE TABLE movie_names (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    mov_name VARCHAR(30)
+);
+
+CREATE TABLE reviews (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    review TEXT,
+    movie_id VARCHAR(30),
+    FOREIGN KEY (movie_id)
+    REFERENCES movie_names(id)
+    ON DELETE SET NULL
+);
