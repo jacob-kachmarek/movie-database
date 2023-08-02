816b4a3b7fd215c8d9e3b5cb2a5f54bff05fe829
unknown <samtisme@gmail.com>
2023-08-02 13:19:16 -0700

    added data in seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO movie_names (mov_name)
+VALUES ("The Matrix"),("The Godfather"),("About time"),("Stranger than Fiction"),("Ratatoullie"),("The A Team");
+
+INSERT INTO reviews (review, movie_id)
+VALUES ("John wick teleporting", 1),("Old guy is Italian", 2),("Time travel", 3),("Will Ferrel plays guitar", 4),("Rat is a chef", 5),("Mr T", 6);
